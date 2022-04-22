6ead0ef3ae48645a7bc7840ac6d0ca1c31d93a1f
silkesssji <kastrdast@mail.ru>
2022-04-23 02:26:40 +0400

    Add tickets

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,38 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './scss/index.scss';
 import logoImg from './img/logo.svg';
+import s7Logo from "./img/s7logo.svg";
 import classNames from 'classnames';
+import { Server } from "miragejs";
+import ticketsResponse1 from "./tikets.json";
+import ticketsResponse2 from "./tikets2.json";
 
+let fetchCounter = 0;
+new Server({
+  routes() {
+    this.namespace = "api";
+    this.get("/users/", () => {
+        if (fetchCounter === 0) {
+            fetchCounter += 1;
+            return ticketsResponse1;
+        } else {
+            fetchCounter += 1;
+            return ticketsResponse2;
+        } 
+    });
+}});
+
+let ticketsResponse;
+
+fetch("/api/users/").then(response => {
+      if (!response.ok) throw Error(response.statusText);
+      return response.json();
+    }).then((json) => {
+        console.log(json);
+        ticketsResponse = json;
+    });
+
+console.log(ticketsResponse)
 
 class PageSwitch extends React.Component {
     handlingChange = () => {
@@ -14,7 +44,7 @@ class PageSwitch extends React.Component {
             <div className={classNames('pageSwitch__container', this.props.checked?'pageSwitch__container-checked':'pageSwitch__container')} onClick={() => this.handlingChange()}>
             <h2 className='pageSwitch__heading'>{this.props.label}</h2>
             <div className='pageSwitch__content'></div>
-                <input className='pageSwitch__radio' type="radio" name={this.props.name} checked={this.props.checked} value={this.props.value} onChange={this.props.onChange}></input>
+                <input className='pageSwitch__radio' type="button" name={this.props.name} checked={this.props.checked} value={this.props.value} onChange={this.props.onChange}></input>
             </div>
         )
     }
@@ -76,7 +106,7 @@ class PageController extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            pageControllerValue: 'cheapest',
+            pageControllerValue: 0,
         }
     }
     handlingRadioChange = (i) => {
@@ -93,11 +123,52 @@ class PageController extends React.Component {
     }
 }
 
+class Ticket extends React.Component {
+    render() {
+        return(
+            <div className='ticketBody'>
+                <div className='ticketBody__headingContainer'>
+                    <h2 className='ticketBody__heading'>17 500 Р</h2>
+                    <img src={s7Logo} alt="logo" width="150px" height="60px"/>
+                </div>
+                <div className='ticketBody__data'>
+                    <div className='ticketBody__dataBlock'>
+                        <h2 className='ticketBody__dataHeading'>MOW-HKT</h2>
+                        <h2 className='ticketBody__dataTime'>10:45-08:00</h2>
+                    </div>
+                    <div className='ticketBody__dataBlock'>
+                        <h2 className='ticketBody__dataHeading'>В пути</h2>
+                        <h2 className='ticketBody__dataTime'>21ч 15м</h2>
+                    </div>
+                    <div className='ticketBody__dataBlock'>
+                        <h2 className='ticketBody__dataHeading'>2 пересадки</h2>
+                        <h2 className='ticketBody__dataTime'>HKG, JNB</h2>
+                    </div>
+                </div>
+                <div className='ticketBody__data'>
+                    <div className='ticketBody__dataBlock'>
+                        <h2 className='ticketBody__dataHeading'>MOW-HKT</h2>
+                        <h2 className='ticketBody__dataTime'>10:45-08:00</h2>
+                    </div>
+                    <div className='ticketBody__dataBlock'>
+                        <h2 className='ticketBody__dataHeading'>В пути</h2>
+                        <h2 className='ticketBody__dataTime'>21ч 15м</h2>
+                    </div>
+                    <div className='ticketBody__dataBlock'>
+                        <h2 className='ticketBody__dataHeading'>2 пересадки</h2>
+                        <h2 className='ticketBody__dataTime'>HKG, JNB</h2>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
 class App extends React.Component {
     render() {
         return(
             <div className='main'>
-                <img className='logo' src={logoImg} alt='logo' width='60px' height='60px'/>
+                <a href="https://www.aviasales.ru/"><img className='logo' src={logoImg} alt='logo' width='60px' height='60px'/></a>
                 <div className='content'>
                     <div className='filterPanel'>
                         <h2 className='filterPanel__heading'>Количество пересадок</h2>
@@ -105,6 +176,15 @@ class App extends React.Component {
                     </div>
                     <div className='biletsPanel'>
                         <PageController/>
+                    </div>
+                    <div className='ticketsPanel'>
+                        <div className='ticketsPanel__container'>
+                            <Ticket/>
+                            <Ticket/>
+                            <Ticket/>
+                            <Ticket/>
+                        </div>
+                        <button type='button' className='moreResultsButton'>Показать еще 5 билетов</button>
                     </div>
                 </div>
             </div>
