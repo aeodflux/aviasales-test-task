1d73e07406a4ee8237629af6f0a6768f126bd60f
silkesssji <kastrdast@mail.ru>
2022-04-22 12:10:05 +0400

    Finish filters

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import './scss/index.scss';
 import logoImg from './img/logo.svg';
 import classNames from 'classnames';
 
+
 class PageSwitch extends React.Component {
     handlingChange = () => {
         this.props.onChange();
@@ -19,11 +20,63 @@ class PageSwitch extends React.Component {
     }
 }
 
-class App extends React.Component {
+class NewCheckbox extends React.Component {
+    handlingChange = () => {
+        this.props.onChange();
+    }
+    render() {
+        return(
+            <div className={classNames("filterPanel__checkbox", this.props.checked?"filterPanel__checkbox-checked":"filterPanel__checkbox")} onClick={this.handlingChange}>
+                <input className='filterPanel__input' type="checkbox" id={this.props.id} checked={this.props.checked} onChange={this.props.onChange}/>
+                <div className='filterPanel__content' onClick={this.handlingChange}></div>
+                <label className='filterPanel__label' htmlFor={this.props.id} onClick={(e) => e.preventDefault()}>{this.props.label}</label>
+            </div>
+        )
+    }
+}
+
+class FilterPanel extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            pageControllerValue: 0,
+            allChecked: false,
+            withoutTransfer: false,
+            oneTransfer: true,
+            twoTransfer: false,
+            threeTransfer: false,
+        }
+    }
+    handlingCheckboxChange = (name) => {
+        let value = this.state[name];
+        if (name === "allChecked") {
+            this.setState({allChecked: !value});
+            this.setState({withoutTransfer: !value});
+            this.setState({oneTransfer: !value})
+            this.setState({twoTransfer: !value})
+            this.setState({threeTransfer: !value})
+        } else {
+            this.setState({allChecked: false})
+            this.setState({[name]: !value});
+        }
+    }
+    render() {
+        return(
+            <div className="filterPanel__container">
+                <NewCheckbox id='checkboxFilterPanel1' label="Все" checked={this.state.allChecked} value="allChecked" onChange={() => this.handlingCheckboxChange("allChecked")}/>
+                <NewCheckbox id='checkboxFilterPanel2' label="Без пересадок" checked={this.state.withoutTransfer} value="withoutTransfer" onChange={() => this.handlingCheckboxChange("withoutTransfer")}/>
+                <NewCheckbox id='checkboxFilterPanel3' label="1 пересадка" checked={this.state.oneTransfer} value="oneTransfer" onChange={() => this.handlingCheckboxChange("oneTransfer")}/>
+                <NewCheckbox id='checkboxFilterPanel4' label="2 пересадки" checked={this.state.twoTransfer} value="twoTransfer" onChange={() => this.handlingCheckboxChange("twoTransfer")}/>
+                <NewCheckbox id='checkboxFilterPanel5' label="3 пересадки" checked={this.state.threeTransfer} value="threeTransfer" onChange={() => this.handlingCheckboxChange("threeTransfer")}/>
+            </div>
+        )
+    }
+}
+
+class PageController extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pageControllerValue: 'cheapest',
         }
     }
     handlingRadioChange = (i) => {
@@ -31,25 +84,27 @@ class App extends React.Component {
     }
     render() {
         return(
+            <div className="pageController">
+                <PageSwitch name="radioBiletsPanel" value='cheapest' label="Самый дешевый" checked={this.state.pageControllerValue==='cheapest'} onChange={() => this.handlingRadioChange('cheapest')}></PageSwitch>
+                <PageSwitch name="radioBiletsPanel" value='fastest' label="Самый быстрый" checked={this.state.pageControllerValue==='fastest'} onChange={() => this.handlingRadioChange('fastest')}></PageSwitch>
+                <PageSwitch name="radioBiletsPanel" value='optimal' label="Оптимальный" checked={this.state.pageControllerValue==='optimal'} onChange={() => this.handlingRadioChange('optimal')}></PageSwitch>
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return(
             <div className='main'>
                 <img className='logo' src={logoImg} alt='logo' width='60px' height='60px'/>
                 <div className='content'>
                     <div className='filterPanel'>
                         <h2 className='filterPanel__heading'>Количество пересадок</h2>
-                        <div className="filterPanel__container">
-                            <div className="filterPanel__checkbox"><input type="checkbox" id='checkboxFilterPanel1'/><label className='filterPanel__label' htmlFor="checkboxFilterPanel1">Все</label></div>
-                            <div className="filterPanel__checkbox"><input type="checkbox" id='checkboxFilterPanel2'/><label className='filterPanel__label' htmlFor="checkboxFilterPanel2">Без пересадок</label></div>
-                            <div className="filterPanel__checkbox"><input type="checkbox" id='checkboxFilterPanel3'/><label className='filterPanel__label' htmlFor="checkboxFilterPanel3">1 пересадка</label></div>
-                            <div className="filterPanel__checkbox"><input type="checkbox" id='checkboxFilterPanel4'/><label className='filterPanel__label' htmlFor="checkboxFilterPanel4">2 пересадки</label></div>
-                            <div className="filterPanel__checkbox"><input type="checkbox" id='checkboxFilterPanel5'/><label className='filterPanel__label' htmlFor="checkboxFilterPanel5">3 пересадки</label></div>
-                        </div>
+                        <FilterPanel/>
                     </div>
                     <div className='biletsPanel'>
-                        <div className='pageController'>
-                            <PageSwitch name="radioBiletsPanel" value={0} label="Самый дешевый" checked={this.state.pageControllerValue===0} onChange={() => this.handlingRadioChange(0)}></PageSwitch>
-                            <PageSwitch name="radioBiletsPanel" value={1} label="Самый быстрый" checked={this.state.pageControllerValue===1} onChange={() => this.handlingRadioChange(1)}></PageSwitch>
-                            <PageSwitch name="radioBiletsPanel" value={2} label="Оптимальный" checked={this.state.pageControllerValue===2} onChange={() => this.handlingRadioChange(2)}></PageSwitch>
-                        </div>
+                        <PageController/>
                     </div>
                 </div>
             </div>
