ee5868514d2d47d9f270db838107e24898568076
silkesssji <kastrdast@mail.ru>
2022-04-27 17:15:06 +0400

    Add skeletons

--- src/index.jsx
+++ src/index.jsx
@@ -187,7 +187,40 @@ class Ticket extends React.Component {
         )
     }
 }
-
+const skeletonTicket = <div className='ticketBody skeletonTicket'>
+<div className='ticketBody__headingContainer'>
+    <div className='ticketBody__heading'></div>
+    <div alt="logo" width="150px" height="60px" className="skeletonTicket__logo"/>
+</div>
+<div className='ticketBody__data'>
+    <div className='ticketBody__dataBlock dataBlock__time'>
+        <div className='ticketBody__dataHeading'></div>
+        <div className='ticketBody__dataTime'></div>
+    </div>
+    <div className='ticketBody__dataBlock dataBlock__path'>
+        <div className='ticketBody__dataHeading'></div>
+        <div className='ticketBody__dataTime'></div>
+    </div>
+    <div className='ticketBody__dataBlock'>
+    <div className="ticketBody__dataHeading"></div>
+        <div className='ticketBody__dataTime'></div>
+    </div>
+</div>
+<div className='ticketBody__data'>
+    <div className='ticketBody__dataBlock dataBlock__time'>
+        <div className='ticketBody__dataHeading'></div>
+        <div className='ticketBody__dataTime'></div>
+    </div>
+    <div className='ticketBody__dataBlock dataBlock__path'>
+        <div className='ticketBody__dataHeading'></div>
+        <div className='ticketBody__dataTime'></div>
+    </div>
+    <div className='ticketBody__dataBlock'>
+        <div className="ticketBody__dataHeading"></div>
+        <div className='ticketBody__dataTime'></div>
+    </div>
+</div>
+</div>
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -241,7 +274,16 @@ class App extends React.Component {
             } else if ((this.state.data) && !(this.state.loading)) {
                 return <button type='moreResultsButton' className='moreResultsButton' onClick={this.showMoreTickets}>Показать еще билеты</button>;
             } else {
-                return <div className='preloader'></div>
+                return <div
+                //  className='preloader'
+                 >
+                    {skeletonTicket}
+                    {skeletonTicket}
+                    {skeletonTicket}
+                    {skeletonTicket}
+                    {skeletonTicket}
+                    {skeletonTicket}
+                </div>
             }
         }
         const cheapestSorting = (a, b) => {
@@ -272,7 +314,7 @@ class App extends React.Component {
                     } else if (this.state.pageControllerValue === 'optimal') {
                         return optimalSorting(a, b)
                     } else {
-                        return 
+                        return this
                     }  
                 }
                 ).map((ticket, index) => <Ticket key={index} value={ticket}/>)))
