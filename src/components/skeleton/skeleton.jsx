93fdbfab637c188e4ba7e7ad6144b58464b469e6
silkesssji <kastrdast@mail.ru>
2022-05-12 21:07:41 +0400

    Fix skeletons

--- src/components/skeleton/skeleton.jsx
+++ src/components/skeleton/skeleton.jsx
@@ -8,45 +8,41 @@ export class SkeletonTicket extends React.Component {
         this.count = this.props.count;
     }
     render() {
-        let result = [];
-        for (let i = 0; i < this.count; i++) {
-            result.push(
-                <div className='ticketBody skeletonTicket' key={i}>
-                    <div className='ticketBody__headingContainer'>
-                        <div className='ticketBody__heading'></div>
-                        <div alt="logo" width="150px" height="60px" className="skeletonTicket__logo"/>
+        return (Array.from({ length: this.props.count }).map((el, indx) => 
+            <div className='ticketBody skeletonTicket' key={indx}>
+                <div className='ticketBody__headingContainer'>
+                    <div className='ticketBody__heading'></div>
+                    <div alt="logo" width="150px" height="60px" className="skeletonTicket__logo"/>
+                </div>
+                <div className='ticketBody__data'>
+                    <div className='ticketBody__dataBlock dataBlock__time'>
+                        <div className='ticketBody__dataHeading'></div>
+                        <div className='ticketBody__dataTime'></div>
                     </div>
-                    <div className='ticketBody__data'>
-                        <div className='ticketBody__dataBlock dataBlock__time'>
-                            <div className='ticketBody__dataHeading'></div>
-                            <div className='ticketBody__dataTime'></div>
-                        </div>
-                        <div className='ticketBody__dataBlock dataBlock__path'>
-                            <div className='ticketBody__dataHeading'></div>
-                            <div className='ticketBody__dataTime'></div>
-                        </div>
-                        <div className='ticketBody__dataBlock'>
+                    <div className='ticketBody__dataBlock dataBlock__path'>
+                        <div className='ticketBody__dataHeading'></div>
+                        <div className='ticketBody__dataTime'></div>
+                    </div>
+                    <div className='ticketBody__dataBlock'>
+                    <div className="ticketBody__dataHeading"></div>
+                        <div className='ticketBody__dataTime'></div>
+                    </div>
+                </div>
+                <div className='ticketBody__data'>
+                    <div className='ticketBody__dataBlock dataBlock__time'>
+                        <div className='ticketBody__dataHeading'></div>
+                        <div className='ticketBody__dataTime'></div>
+                    </div>
+                    <div className='ticketBody__dataBlock dataBlock__path'>
+                        <div className='ticketBody__dataHeading'></div>
+                        <div className='ticketBody__dataTime'></div>
+                    </div>
+                    <div className='ticketBody__dataBlock'>
                         <div className="ticketBody__dataHeading"></div>
-                            <div className='ticketBody__dataTime'></div>
-                        </div>
+                        <div className='ticketBody__dataTime'></div>
                     </div>
-                    <div className='ticketBody__data'>
-                        <div className='ticketBody__dataBlock dataBlock__time'>
-                            <div className='ticketBody__dataHeading'></div>
-                            <div className='ticketBody__dataTime'></div>
-                        </div>
-                        <div className='ticketBody__dataBlock dataBlock__path'>
-                            <div className='ticketBody__dataHeading'></div>
-                            <div className='ticketBody__dataTime'></div>
-                        </div>
-                        <div className='ticketBody__dataBlock'>
-                            <div className="ticketBody__dataHeading"></div>
-                            <div className='ticketBody__dataTime'></div>
-                        </div>
-                    </div>
-                </div> 
-            )
-        }
-        return result;
+                </div>
+            </div>)
+        )
     }
 }
